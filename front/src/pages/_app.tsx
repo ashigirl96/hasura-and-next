983ec238d2014d835315fc544bd68aef411eadb2
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2021-08-21 01:24:13 +0900

    Initial Commit

---
+++ front/src/pages/_app.tsx
@@ -0,0 +1,43 @@
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from '@apollo/client'
+import { AppProps } from 'next/app'
+import '@/styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+
+import { extendTheme } from '@chakra-ui/react'
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+const theme = extendTheme({ colors })
+
+const createApolloClient = () => {
+  return new ApolloClient({
+    link: new HttpLink({
+      uri: 'http://localhost:8080/v1/graphql',
+    }),
+    cache: new InMemoryCache(),
+  })
+}
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const client = createApolloClient()
+
+  return (
+    <ApolloProvider client={client}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ApolloProvider>
+  )
+}
+
+export default MyApp
---
+++ front/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next'
+
+const Index: NextPage = () => {
+  return (
+    <div>Hello</div>
+  )
+}
+
+export default Index
